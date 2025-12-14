f8c555d542afd16188ba27557eecbcecbfda2dc3
Alwinpro <alwinmanoj38@gmail.com>
2025-12-14 15:15:47 +0530

    Add Supabase-only admin signup page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import ManagementDashboard from './pages/ManagementDashboard';
 import TeacherDashboard from './pages/TeacherDashboard';
 import StudentDashboard from './pages/StudentDashboard';
 import NotFoundPage from './pages/NotFoundPage';
+import AdminSignup from './pages/AdminSignup';
 
 // Protected Route Component
 const ProtectedRoute = ({ children, allowedRoles }) => {
@@ -47,6 +48,9 @@ function App() {
           <Route path="/login/management" element={<LoginPage role="management" />} />
           <Route path="/login/teacher" element={<LoginPage role="teacher" />} />
           <Route path="/login/student" element={<LoginPage role="student" />} />
+
+          {/* Admin Signup Route - One Time Use */}
+          <Route path="/admin/signup" element={<AdminSignup />} />
 
           {/* Dashboards */}
           <Route
